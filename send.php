b8d5346bef779ab357a9244ff2de1880545c33cf
zbb2227056678 <2227056678@qq.com>
2019-08-26 18:47:21 +0800

    Update send.php

--- send.php
+++ send.php
@@ -17,7 +17,7 @@ $mail->isSMTP();
 // smtp需要鉴权 这个必须是true
 $mail->SMTPAuth = true;
 // 链接qq域名邮箱的服务器地址
-$mail->Host = 'smtp.qq.com';//nmqsfbzqhotzecea
+$mail->Host = 'smtp.qq.com';
 // 设置使用ssl加密方式登录鉴权
 $mail->SMTPSecure = 'ssl';
 // 设置ssl连接smtp服务器的远程服务器端口号
@@ -27,15 +27,15 @@ $mail->CharSet = 'UTF-8';
 // 设置发件人昵称 显示在收件人邮件的发件人邮箱地址前的发件人姓名
 $mail->FromName = 'hahhaahhah';
 // smtp登录的账号 QQ邮箱即可
-$mail->Username = '2227056678@qq.com';
+$mail->Username = 'xxxx@qq.com';
 // smtp登录的密码 使用生成的授权码
-$mail->Password = 'nmqsfbzqhotzecea';
+$mail->Password = 'xxxxxxxx';
 // 设置发件人邮箱地址 同登录账号
-$mail->From = '2227056678@qq.com';
+$mail->From = 'xxxx@qq.com';
 // 邮件正文是否为html编码 注意此处是一个方法
 $mail->isHTML(true);
 // 设置收件人邮箱地址
-$mail->addAddress('2227056678@qq.com');
+$mail->addAddress('xxxx@qq.com');
 // 添加多个收件人 则多次调用方法即可
 //$mail->addAddress('87654321@163.com');
 // 添加该邮件的主题
